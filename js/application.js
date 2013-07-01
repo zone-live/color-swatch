3acd1f4faadd96c2e2305b37dfad65ab07da4cfb
António Regadas <apregadas@gmail.com>
2013-07-01 23:02:46 +0100

    Repository cleanned

--- js/application.js
+++ js/application.js
@@ -1,4 +1,3 @@
-// Some general UI pack related JS
 // Extend JS String with repeat method
 String.prototype.repeat = function(num) {
     return new Array(num + 1).join(this);
@@ -66,3 +65,81 @@ String.prototype.repeat = function(num) {
   });
   
 })(jQuery);
+
+
+//App code! 
+$(document).ready(function() {
+     $('#colorpicker').farbtastic('#color');
+ });
+
+function darkestColor(col, amt) {
+  
+    var usePound = false;
+  
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+ 
+    var num = parseInt(col,16);
+ 
+    var r = (num >> 16) + amt;
+ 
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+ 
+    var b = ((num >> 8) & 0x00FF) + amt;
+ 
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+ 
+    var g = (num & 0x0000FF) + amt;
+ 
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+ 
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+  
+}
+
+function colorCtrl($scope) {
+  
+  $('#generate').live('click', function() {
+
+    $('.demo-panel-title').fadeIn();
+
+    var color1 = $scope.color1;
+    var color2 = $scope.color2;
+    var color3 = $scope.color3;
+
+    var darkColor1 = darkestColor(color1, -20);
+    var darkColor2 = darkestColor(color2, -20);
+    var darkColor3 = darkestColor(color3, -20);
+
+    $('.mbl').append('<article><div class="span2">' +
+                        '<dl class="palette" style="background-color:'+color1+';">' +
+                          '<dt>'+ color1 +'</dt>' +
+                        '</dl>' +
+                        '<dl class="palette" style="background-color:'+darkColor1+';">' +
+                          '<dt>'+ darkColor1 +'</dt>' +
+                        '</dl>' +
+                    '</div>' + 
+                    '<div class="span2">' +
+                        '<dl class="palette" style="background-color:'+color2+';">' +
+                          '<dt>'+ color2 +'</dt>' +
+                        '</dl>' +
+                        '<dl class="palette" style="background-color:'+darkColor2+';">' +
+                          '<dt>'+ darkColor2 +'</dt>' +
+                        '</dl>' +
+                    '</div>' +
+                    '<div class="span2">' +
+                        '<dl class="palette" style="background-color:'+color3+';">' +
+                          '<dt>'+ color3 +'</dt>' +
+                        '</dl>' +
+                        '<dl class="palette" style="background-color:'+darkColor3+';">' +
+                          '<dt>'+ darkColor3 +'</dt>' +
+                        '</dl>' +
+                    '</div></article>');
+  });
+
+}
